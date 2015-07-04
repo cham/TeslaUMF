0fe6d1bdd154bd36411b22b89f4c02235f66e549
Dan Neame <danneame@gmail.com>
2015-07-04 23:37:53 +0100

    main file spec and logic

---
+++ public/test/main/main.spec.js
@@ -0,0 +1,63 @@
+'use strict';
+define([
+    'bogus'
+],
+function(
+    bogus
+){
+
+    var sandbox = sinon.sandbox.create();
+
+    describe('main', function(){
+        var main;
+        var newAppViewStub;
+        var appViewInstance;
+        var appendChildSpy;
+
+        before(function(done){
+            appViewInstance = {
+                render: sandbox.stub(),
+                el: document.createElement('div')
+            };
+
+            newAppViewStub = sandbox.stub().returns(appViewInstance);
+
+            bogus.stub('views/AppView', newAppViewStub);
+
+            bogus.requireWithStubs('main', function(module){
+                main = module;
+                done();
+            });
+        });
+
+        beforeEach(function(){
+            appendChildSpy = sandbox.spy(HTMLElement.prototype, 'appendChild');
+        });
+
+        afterEach(function(){
+            sandbox.restore();
+        });
+
+        after(function(){
+            appViewInstance.el.remove();
+        });
+
+        it('creates a single new AppView', function(){
+            expect(newAppViewStub.calledOnce).toEqual(true);
+        });
+
+        it('appends the new AppView\'s el to the document body', function(){
+            var childNodes = Array.prototype.slice.call(document.body.children);
+            var elNodesInDocument = childNodes.filter(function(node){
+                return node === appViewInstance.el;
+            });
+
+            expect(elNodesInDocument.length).toEqual(1);
+        });
+
+        it('renders the new AppView once', function(){
+            expect(appViewInstance.render.calledOnce).toEqual(true);
+        });
+    });
+
+});
