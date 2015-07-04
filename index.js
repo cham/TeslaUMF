faf0152204681cc192558c9542091506a17e54aa
Dan Neame <danneame@gmail.com>
2015-07-04 20:36:54 +0100

    added routemaster and test route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 var express = require('express');
+var routemaster = require('routemaster');
 var http = require('http');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,10 @@ var serverPort = 3200;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(routemaster({
+    directory: './routes',
+    Router: express.Router
+}));
 
 server.listen(serverPort, function(){
     console.info('UMF listening on port', serverPort);
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(router){
+    router
+        .get('/', function(req, res){
+            res.send('index');
+        });
+};
