3b2bb88bcecd810d352ccc2c5ede5ef6637f0d19
Dan Neame <danneame@gmail.com>
2015-07-05 17:07:25 +0100

    simplified main, split out requireConfig

---
+++ public/js/requireConfig.js
@@ -0,0 +1,16 @@
+'use strict';
+
+define(function(){
+
+    require.config({
+        paths: {
+            'bogus': '/js/vendor/bogus/bogus',
+            'q': '/js/vendor/q/q',
+            'pubsub': '/js/vendor/pubsub-js/src/pubsub',
+            'hogan': '/js/vendor/requirejs-hogan-plugin/hogan',
+            'hgn': '/js/vendor/requirejs-hogan-plugin/hgn',
+            'text': '/js/vendor/requirejs-text/text'
+        }
+    });
+
+});
