e40352efd97e065ecbce010a93422458f9f7d91c
Dan Neame <danneame@gmail.com>
2015-07-19 02:22:05 +0100

    wire up client side thread controller

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,30 @@
 
 define([
     'minivents',
-    'controllers/listing'
+    'controllers/listing',
+    'controllers/thread'
 ],
 function(
     Minivents,
-    listing
+    listing,
+    thread
 ){
 
     var listingEvents = new Minivents();
+    var threadEvents = new Minivents();
+
     listing.load({
         el: document.body,
         eventBus: listingEvents
     });
 
-    listingEvents.on('load:thread', function(threadId){
+    listingEvents.on('load:thread', function(threadUrlName){
         listing.unload();
+        thread.load({
+            el: document.body,
+            eventBus: threadEvents,
+            threadUrlName: threadUrlName
+        });
     });
 
 });
--- public/test/main/app.spec.js
+++ public/test/main/app.spec.js
@@ -1,10 +1,12 @@
 'use strict';
 define([
     'controllers/listing',
+    'controllers/thread',
     'minivents'
 ],
 function(
     listing,
+    thread,
     Minivents
 ){
 
@@ -13,13 +15,22 @@ function(
 
     describe('app', function(){
         var listingLoadStub;
+        var listingUnloadStub;
+        var threadLoadStub;
+        var onStub;
 
         before(function(done){
             listingLoadStub = sandbox.stub(listing, 'load');
+            listingUnloadStub = sandbox.stub(listing, 'unload');
+            threadLoadStub = sandbox.stub(thread, 'load');
             require(['app'], function(module){
                 app = module;
                 done();
             });
+        });
+
+        beforeEach(function(){
+            threadLoadStub.reset();
         });
 
         it('calls load on the listing controller once', function(){
@@ -44,6 +55,51 @@ function(
 
         it('passes a Minivent object as the eventBus', function(){
             expect(listingLoadStub.args[0][0].eventBus instanceof Minivents).toEqual(true);
+        });
+
+        describe('when a load:thread event fires on the listing event bus', function(){
+            var eventBus;
+
+            beforeEach(function(){
+                eventBus = listingLoadStub.args[0][0].eventBus;
+                eventBus.emit('load:thread', 'some-thread');
+            });
+
+            it('unloads the listing controller', function(){
+                expect(listingUnloadStub.calledOnce).toEqual(true);
+            });
+
+            it('calls load on the thread controller', function(){
+                expect(threadLoadStub.calledOnce).toEqual(true);
+            });
+
+            it('passes options to thread controller load', function(){
+                expect(typeof threadLoadStub.args[0][0]).toEqual('object');
+            });
+
+            it('passes an el option', function(){
+                expect(threadLoadStub.args[0][0].el).toBeDefined();
+            });
+
+            it('passes the document body as the value for el', function(){
+                expect(threadLoadStub.args[0][0].el).toEqual(document.body);
+            });
+
+            it('passes an eventBus option', function(){
+                expect(threadLoadStub.args[0][0].eventBus).toBeDefined();
+            });
+
+            it('passes a Minivent object as the eventBus', function(){
+                expect(threadLoadStub.args[0][0].eventBus instanceof Minivents).toEqual(true);
+            });
+
+            it('passes a threadUrlName option', function(){
+                expect(threadLoadStub.args[0][0].threadUrlName).toBeDefined();
+            });
+
+            it('passes the threadUrlName from the load:thread event for the threadUrlName value', function(){
+                expect(threadLoadStub.args[0][0].threadUrlName).toEqual('some-thread');
+            });
         });
     });
 
