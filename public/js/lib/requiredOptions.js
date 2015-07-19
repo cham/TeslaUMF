bf9b226a8d86ec3a3b732ae9861a1660f4175f1c
Dan Neame <danneame@gmail.com>
2015-07-19 02:00:28 +0100

    requiredOptions

---
+++ public/js/lib/requiredOptions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+define(function(){
+
+    return function requiredOptions(options, id, required){
+        required.forEach(function(key){
+            if(!options[key]){
+                throw new Error(id + ' requires ' + key);
+            }
+        });
+    };
+
+});
---
+++ public/test/lib/requiredOptions.spec.js
@@ -0,0 +1,76 @@
+'use strict';
+define([
+    'lib/requiredOptions'
+],
+function(
+    requiredOptions
+){
+    var sandbox = sinon.sandbox.create();
+
+    describe('requiredOptions', function(){
+        describe('when the options do not match the criteria', function(){
+            var options;
+            var required;
+
+            beforeEach(function(){
+                options = {};
+                required = [
+                    'foo',
+                    'bar'
+                ];
+            });
+
+            it('throws an error for the first key that fails', function(){
+                var expectedError = 'some-module requires foo';
+                expect(function(){
+                    return requiredOptions(options, 'some-module', required);
+                }).toThrow(expectedError);
+            });
+        });
+
+        describe('when some of the options match the criteria', function(){
+            var options;
+            var required;
+
+            beforeEach(function(){
+                options = {
+                    foo: 5
+                };
+                required = [
+                    'foo',
+                    'bar'
+                ];
+            });
+
+            it('throws an error for the first key that fails', function(){
+                var expectedError = 'some-module requires bar';
+                expect(function(){
+                    return requiredOptions(options, 'some-module', required);
+                }).toThrow(expectedError);
+            });
+        });
+
+        describe('when all of the options match the criteria', function(){
+            var options;
+            var required;
+
+            beforeEach(function(){
+                options = {
+                    foo: 5,
+                    bar: true
+                };
+                required = [
+                    'foo',
+                    'bar'
+                ];
+            });
+
+            it('does not throw an error', function(){
+                expect(function(){
+                    return requiredOptions(options, 'some-module', required);
+                }).not.toThrow();
+            });
+        });
+    });
+
+});
